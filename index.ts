c6014ee19b7876fc5c121911062d88776aa38296
pedrohgaffonso <pedrohgaffonso@gmail.com>
2022-08-27 18:25:08 -0300

    Modified Typescript index.ts file feeding the HTML page

--- index.ts
+++ index.ts
@@ -1,6 +1,20 @@
 // Import stylesheets
 import './style.css';
+import { Person } from './person';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let me: Person = new Person();
+me.firstname = 'Pedro';
+me.middlename = 'Henrique Garcia';
+me.lastname = 'Affonso';
+me.age = 27;
+me.email.push('pedrohgaffonso@gmail.com');
+me.email.push('pedro.affonso.77@gmail.com');
+
+const appname: HTMLElement = document.getElementById('person-name');
+appname.innerHTML = me.getFullName();
+const appmail: HTMLElement = document.getElementById('person-emails');
+appmail.innerHTML = me.email.toString();
+const appage: HTMLElement = document.getElementById('person-age');
+appage.innerHTML = me.age.toString();
+const appyear: HTMLElement = document.getElementById('person-birthday');
+appyear.innerHTML = me.getBirtdayYear().toString();
